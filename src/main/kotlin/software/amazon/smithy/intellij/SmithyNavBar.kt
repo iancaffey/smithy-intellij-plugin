48b2a426a854e2fabf6b41db25479413a3fdcf30
Ian Caffey <mail@iancaffey.com>
2022-07-05 22:25:16 -0500

    Add nav bar which supports navigating to any structure view element in a Smithy file.

---
+++ src/main/kotlin/software/amazon/smithy/intellij/SmithyNavBar.kt
@@ -0,0 +1,18 @@
+package software.amazon.smithy.intellij
+
+import com.intellij.ide.navigationToolbar.NavBarModelExtension
+import com.intellij.ide.navigationToolbar.StructureAwareNavBarModelExtension
+import software.amazon.smithy.intellij.psi.SmithyElement
+import software.amazon.smithy.intellij.psi.SmithyNamedElement
+
+/**
+ * A [NavBarModelExtension] for [Smithy](https://awslabs.github.io/smithy).
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+class SmithyNavBar : StructureAwareNavBarModelExtension() {
+    override val language = SmithyLanguage
+    override fun getPresentableText(value: Any?) = (value as? SmithyNamedElement)?.name
+    override fun getIcon(value: Any?) = (value as? SmithyElement)?.getIcon(0)
+}
