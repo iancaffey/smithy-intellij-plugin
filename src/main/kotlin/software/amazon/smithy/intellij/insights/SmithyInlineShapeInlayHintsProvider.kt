bbcec811bf5e154e4af2b3c292ad76f3d818f5bb
Ian Caffey <mail@iancaffey.com>
2022-08-09 22:08:47 -0500

    Improved the anchor positioning for the inline i/o shape name hint to align with where the name would be positioned if explicitly provided.

--- src/main/kotlin/software/amazon/smithy/intellij/insights/SmithyInlineShapeInlayHintsProvider.kt
+++ src/main/kotlin/software/amazon/smithy/intellij/insights/SmithyInlineShapeInlayHintsProvider.kt
@@ -3,9 +3,13 @@ package software.amazon.smithy.intellij.insights
 import com.intellij.codeInsight.hints.InlayInfo
 import com.intellij.codeInsight.hints.InlayParameterHintsProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil.getChildOfType
 import software.amazon.smithy.intellij.SmithyLanguage
+import software.amazon.smithy.intellij.psi.SmithyContainerBody
 import software.amazon.smithy.intellij.psi.SmithyInput
+import software.amazon.smithy.intellij.psi.SmithyMixins
 import software.amazon.smithy.intellij.psi.SmithyOutput
+import software.amazon.smithy.intellij.psi.SmithyResourceReference
 
 /**
  * An [InlayParameterHintsProvider] which displays the shape name of inlined [SmithyInput]/[SmithyOutput].
@@ -19,8 +23,14 @@ class SmithyInlineShapeInlayHintsProvider : InlayParameterHintsProvider {
     override fun getBlackListDependencyLanguage() = SmithyLanguage
     override fun getInlayPresentation(inlayText: String) = inlayText
     override fun getParameterHints(element: PsiElement) = when (element) {
-        is SmithyInput -> InlayInfo(element.shapeName, element.body.textOffset)
-        is SmithyOutput -> InlayInfo(element.shapeName, element.body.textOffset)
+        is SmithyInput -> InlayInfo(element.shapeName, findAnchor(element).textOffset)
+        is SmithyOutput -> InlayInfo(element.shapeName, findAnchor(element).textOffset)
         else -> null
     }?.let { listOf(it) } ?: emptyList()
+
+    private fun findAnchor(element: PsiElement) =
+        getChildOfType(element, SmithyResourceReference::class.java)
+            ?: getChildOfType(element, SmithyMixins::class.java)
+            ?: getChildOfType(element, SmithyContainerBody::class.java)
+            ?: element
 }
